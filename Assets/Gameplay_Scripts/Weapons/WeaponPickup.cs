785440fdbbe9d34fa42da8164291e1fa254aeeeb
YasinAk37 <yasinakcam@gmail.com>
2025-12-14 14:50:38 +0300

    Sonahler

--- Assets/Gameplay_Scripts/Weapons/WeaponPickup.cs
+++ Assets/Gameplay_Scripts/Weapons/WeaponPickup.cs
@@ -2,9 +2,18 @@ using UnityEngine;
 
 public class WeaponPickup : MonoBehaviour
 {
-    public WeaponBase weaponPrefab; 
+    public WeaponBase weaponPrefab;
     public string pickupName = "Silah";
 
-    
-}
+    // --- MERMÝ HAFIZASI ---
+    // Varsayýlan -1 (Veri yok demektir, silahýn kendi varsayýlanýný kullanýr)
+    [HideInInspector] public int savedAmmoInMag = -1;
+    [HideInInspector] public int savedReserveAmmo = -1;
 
+    // Silah yere düþerken bu fonksiyonu çaðýrýp mermiyi kaydedeceðiz
+    public void SetAmmoData(int mag, int reserve)
+    {
+        savedAmmoInMag = mag;
+        savedReserveAmmo = reserve;
+    }
+}
